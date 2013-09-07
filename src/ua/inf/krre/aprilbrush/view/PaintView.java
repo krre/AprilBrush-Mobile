6b0ca9f067d5110b1a487252bc9a0421bf245fc5
Vladimir Zarypov <krre31@gmail.com>
2013-09-07 10:36:05 +0300

    brush engine paints the simple dabs

--- src/ua/inf/krre/aprilbrush/view/PaintView.java
+++ src/ua/inf/krre/aprilbrush/view/PaintView.java
@@ -2,22 +2,43 @@ package ua.inf.krre.aprilbrush.view;
 
 import ua.inf.krre.aprilbrush.logic.BrushEngine;
 import android.content.Context;
+import android.graphics.Bitmap;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Paint;
 import android.util.AttributeSet;
 import android.view.MotionEvent;
 import android.view.View;
 
 public class PaintView extends View {
     private BrushEngine engine;
-    
+    private Bitmap bitmap;
+    private Canvas canvas;
+    private Paint bitmapPaint;
+
     public PaintView(Context context, AttributeSet attrs) {
         super(context, attrs);
+        bitmapPaint = new Paint(Paint.DITHER_FLAG);
         engine = new BrushEngine();
     }
 
     @Override
-    public boolean onTouchEvent(MotionEvent event) {
-        engine.paintDab(event);
+    public void onSizeChanged(int w, int h, int oldw, int oldh) {
+        super.onSizeChanged(w, h, oldw, oldh);
+        bitmap = Bitmap.createBitmap(w, h, Bitmap.Config.ARGB_8888);
+        canvas = new Canvas(bitmap);
+    }
 
+    @Override
+    public void onDraw(Canvas canvas) {
+        canvas.drawColor(Color.LTGRAY);
+        canvas.drawBitmap(bitmap, 0, 0, bitmapPaint);
+    }
+
+    @Override
+    public boolean onTouchEvent(MotionEvent event) {
+        engine.paintDab(canvas, event);
+        invalidate();
         return true;
     }
 
