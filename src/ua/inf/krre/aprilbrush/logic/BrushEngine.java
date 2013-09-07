5b97cd13152e202ea4f92d5665df21e6cbcec38a
Vladimir Zarypov <krre@mail.ru>
2013-09-07 10:36:05 +0300

    brush engine paints the simple dabs

--- src/ua/inf/krre/aprilbrush/logic/BrushEngine.java
+++ src/ua/inf/krre/aprilbrush/logic/BrushEngine.java
@@ -1,14 +1,33 @@
 package ua.inf.krre.aprilbrush.logic;
 
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Paint;
 import android.util.Log;
 import android.view.MotionEvent;
 
 public class BrushEngine {
     private final String TAG = "AB";
+    Paint paint;
 
-    public void paintDab(MotionEvent event) {
+    public BrushEngine() {
+        paint = new Paint();
+        paint.setColor(Color.BLACK);
+        paint.setAntiAlias(true);
+    }
+
+    public void paintDab(Canvas canvas, MotionEvent event) {
         float x = event.getX();
         float y = event.getY();
-        Log.d(TAG, "x = " + x + " y = " + y);
+        float radius = 15;
+
+        for (int i = 0; i < event.getHistorySize(); i++) {
+            x = event.getHistoricalX(i);
+            y = event.getHistoricalY(i);
+            //Log.d(TAG, "history x = " + x + " history y = " + y);
+            canvas.drawCircle(x, y, radius, paint);
+        }
+        //Log.d(TAG, "x = " + x + " y = " + y);
+        canvas.drawCircle(x, y, radius, paint);
     }
 }
