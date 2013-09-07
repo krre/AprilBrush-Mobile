6ddad0d15e9120dba1eb9232def434621463af16
Vladimir Zarypov <krre@mail.ru>
2013-09-07 12:35:03 +0300

    brush engine: added spacing parameter

--- src/ua/inf/krre/aprilbrush/logic/BrushEngine.java
+++ src/ua/inf/krre/aprilbrush/logic/BrushEngine.java
@@ -3,31 +3,65 @@ package ua.inf.krre.aprilbrush.logic;
 import android.graphics.Canvas;
 import android.graphics.Color;
 import android.graphics.Paint;
-import android.util.Log;
 import android.view.MotionEvent;
 
 public class BrushEngine {
-    private final String TAG = "AB";
-    Paint paint;
+    private Paint paint;
+    private Canvas canvas;
+
+    private float prevX;
+    private float prevY;
+
+    private int spacing = 50;
+    private int size = 20;
 
     public BrushEngine() {
         paint = new Paint();
         paint.setColor(Color.BLACK);
         paint.setAntiAlias(true);
+        paint.setAlpha(128);
     }
 
-    public void paintDab(Canvas canvas, MotionEvent event) {
+    public void setTouch(Canvas canvas, MotionEvent event) {
+        this.canvas = canvas;
         float x = event.getX();
         float y = event.getY();
-        float radius = 15;
+        canvas.drawCircle(x, y, size, paint);
+        prevX = x;
+        prevY = y;
+    }
 
+    public void paintDab(MotionEvent event) {
+        float x, y;
         for (int i = 0; i < event.getHistorySize(); i++) {
             x = event.getHistoricalX(i);
             y = event.getHistoricalY(i);
-            //Log.d(TAG, "history x = " + x + " history y = " + y);
-            canvas.drawCircle(x, y, radius, paint);
+            interpolateDab(x, y);
         }
-        //Log.d(TAG, "x = " + x + " y = " + y);
-        canvas.drawCircle(x, y, radius, paint);
+        x = event.getX();
+        y = event.getY();
+        interpolateDab(x, y);
+    }
+
+    private void interpolateDab(float x, float y) {
+        double length = Math.sqrt(Math.pow(prevX - x, 2)
+                + Math.pow(prevY - y, 2));
+        float deltaDab = size * spacing / 100;
+        // drawing dabs between the events
+        if (length >= deltaDab) {
+            long numDabs = Math.round(length / deltaDab);
+            double angle = Math.atan2(x - prevX, y - prevY);
+            double deltaX = deltaDab * Math.sin(angle);
+            double deltaY = deltaDab * Math.cos(angle);
+            float betweenX = 0;
+            float betweenY = 0;
+            for (int i = 1; i <= numDabs; i++) {
+                betweenX = (float) (prevX + deltaX * i);
+                betweenY = (float) (prevY + deltaY * i);
+                canvas.drawCircle(betweenX, betweenY, size, paint);
+            }
+            prevX = betweenX;
+            prevY = betweenY;
+        }
     }
 }
