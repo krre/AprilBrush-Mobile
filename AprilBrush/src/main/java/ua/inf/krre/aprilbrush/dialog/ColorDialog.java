d5bcb82efcb8adc2f61dbce62b89c86e62e26a19
Vladimir Zarypov <krre31@gmail.com>
2013-09-29 18:32:05 +0300

    added the HSV SeekBars to the Color Dialog

--- AprilBrush/src/main/java/ua/inf/krre/aprilbrush/dialog/ColorDialog.java
+++ AprilBrush/src/main/java/ua/inf/krre/aprilbrush/dialog/ColorDialog.java
@@ -6,6 +6,7 @@ import android.content.DialogInterface;
 import android.os.Bundle;
 import android.support.v4.app.DialogFragment;
 import android.util.Log;
+import android.view.LayoutInflater;
 
 import ua.inf.krre.aprilbrush.R;
 
@@ -13,11 +14,14 @@ public class ColorDialog extends DialogFragment {
     @Override
     public Dialog onCreateDialog(Bundle savedInstanceState) {
         AlertDialog.Builder builder = new AlertDialog.Builder(getActivity());
+        LayoutInflater inflater = getActivity().getLayoutInflater();
+        builder.setView(inflater.inflate(R.layout.dialog_color, null));
         builder.setMessage(R.string.action_color);
         builder.setPositiveButton(android.R.string.ok, new DialogInterface.OnClickListener() {
             @Override
             public void onClick(DialogInterface dialog, int which) {
                 Log.d("AB", "OK");
+
             }
         });
         builder.setNegativeButton(android.R.string.cancel, new DialogInterface.OnClickListener() {
