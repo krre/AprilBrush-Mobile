b084ea7011ef4c49d5603f519fa6e6dae9a4697e
Vladimir Zarypov <krre31@gmail.com>
2013-09-30 19:06:33 +0300

    created custom sliders for brush settings activity

--- AprilBrush/src/main/java/ua/inf/krre/aprilbrush/activity/BrushSettingsActivity.java
+++ AprilBrush/src/main/java/ua/inf/krre/aprilbrush/activity/BrushSettingsActivity.java
@@ -2,13 +2,24 @@ package ua.inf.krre.aprilbrush.activity;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.widget.LinearLayout;
 
 import ua.inf.krre.aprilbrush.R;
+import ua.inf.krre.aprilbrush.view.SliderView;
 
 public class BrushSettingsActivity extends Activity {
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.brush_settings);
+
+        LinearLayout slidersContainer = (LinearLayout) findViewById(R.id.sliders_container);
+        for (int i = 0; i < 4; i++) {
+            SliderView sliderView = new SliderView(getApplicationContext());
+            sliderView.setTitle("Name");
+            sliderView.setValue(100);
+            slidersContainer.addView(sliderView);
+        }
     }
 }
