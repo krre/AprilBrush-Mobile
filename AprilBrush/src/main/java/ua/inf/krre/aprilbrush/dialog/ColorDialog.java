bf4aa038ff2478ed16daf27c11e58a748b67b2e6
Vladimir Zarypov <krre31@gmail.com>
2013-10-06 13:49:08 +0300

    colorpicker: ring draws with transparent hole in the center

--- AprilBrush/src/main/java/ua/inf/krre/aprilbrush/dialog/ColorDialog.java
+++ AprilBrush/src/main/java/ua/inf/krre/aprilbrush/dialog/ColorDialog.java
@@ -4,7 +4,6 @@ import android.app.AlertDialog;
 import android.app.Dialog;
 import android.content.Context;
 import android.content.DialogInterface;
-import android.content.res.Resources;
 import android.os.Bundle;
 import android.support.v4.app.DialogFragment;
 import android.view.LayoutInflater;
@@ -17,31 +16,7 @@ public class ColorDialog extends DialogFragment {
     @Override
     public Dialog onCreateDialog(Bundle savedInstanceState) {
         Context context = getActivity().getApplicationContext();
-        Resources resources = context.getResources();
 
-        /*
-        LinearLayout layout = new LinearLayout(context);
-        layout.setOrientation(LinearLayout.VERTICAL);
-        int padding = resources.getDimensionPixelSize(R.dimen.activity_all_margin);
-        layout.setPadding(padding, padding, padding, padding);
-
-        BrushEngine brushEngine = BrushEngine.getInstance();
-        List<BrushData.Brush> brushList = brushEngine.getBrushList();
-
-        int colorsPropertyNum = 5;
-        for (int i = brushList.size() - colorsPropertyNum; i < brushList.size(); i++) {
-            SliderView sliderView = new SliderView(context);
-            BrushData.Brush brush = brushList.get(i);
-
-            sliderView.setName(brush.getName());
-            sliderView.setMin(brush.getMinValue());
-            sliderView.setMax(brush.getMaxValue());
-            sliderView.setValue(brush.getCurrentValue());
-            sliderView.setId(i);
-            sliderView.addObserver(brushEngine);
-            layout.addView(sliderView);
-        }
-*/
         LayoutInflater inflater = (LayoutInflater) context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
         LinearLayout linearLayout = (LinearLayout) inflater.inflate(R.layout.colorpicker, null);
 
--- AprilBrush/src/main/java/ua/inf/krre/aprilbrush/view/ColorPickerView.java
+++ AprilBrush/src/main/java/ua/inf/krre/aprilbrush/view/ColorPickerView.java
@@ -1,6 +1,7 @@
 package ua.inf.krre.aprilbrush.view;
 
 import android.content.Context;
+import android.graphics.Bitmap;
 import android.graphics.Canvas;
 import android.graphics.Color;
 import android.graphics.Paint;
@@ -12,14 +13,44 @@ import android.util.AttributeSet;
 import android.view.View;
 
 public class ColorPickerView extends View {
-    private Shader shader;
+    private final float RING_RATIO = 0.7f;
     private Paint paint = new Paint(Paint.ANTI_ALIAS_FLAG);
-    private int x = 200;
-    private int y = 200;
+    private Bitmap bitmap;
+    private int viewSize;
 
     public ColorPickerView(Context context, AttributeSet attrs) {
         super(context, attrs);
+    }
 
+    @Override
+    protected void onSizeChanged(int w, int h, int oldw, int oldh) {
+        viewSize = Math.min(w, h);
+
+        if (viewSize > 0) {
+            createColorRing(viewSize);
+        }
+    }
+
+    private void createColorRing(int width) {
+        bitmap = Bitmap.createBitmap(width, width, Bitmap.Config.ARGB_8888);
+        bitmap.eraseColor(Color.TRANSPARENT);
+
+        int center = width / 2;
+        Shader ringShader = new SweepGradient(center, center, gradientColors(), null);
+
+        Paint paint = new Paint(Paint.ANTI_ALIAS_FLAG);
+        paint.setShader(ringShader);
+        Canvas canvas = new Canvas(bitmap);
+        canvas.drawCircle(center, center, center, paint);
+
+        paint.reset();
+        paint.setAntiAlias(true);
+        paint.setXfermode(new PorterDuffXfermode(PorterDuff.Mode.CLEAR));
+
+        canvas.drawCircle(center, center, Math.round(RING_RATIO * center), paint);
+    }
+
+    int[] gradientColors() {
         int segments = 7;
         int hueMax = 360;
         float hueStep = hueMax / (segments - 1);
@@ -29,18 +60,11 @@ public class ColorPickerView extends View {
             colors[i] = Color.HSVToColor(hsv);
             hsv[0] += hueStep;
         }
-        shader = new SweepGradient(x, y, colors, null);
+        return colors;
     }
 
     @Override
     protected void onDraw(Canvas canvas) {
-        paint.setShader(shader);
-        canvas.drawCircle(x, y, 150, paint);
-        paint.reset();
-        paint.setAntiAlias(true);
-        paint.setXfermode(new PorterDuffXfermode(PorterDuff.Mode.CLEAR));
-        paint.setColor(Color.WHITE);
-        canvas.drawCircle(x, y, 100, paint);
-
+        canvas.drawBitmap(bitmap, 0, 0, paint);
     }
 }
