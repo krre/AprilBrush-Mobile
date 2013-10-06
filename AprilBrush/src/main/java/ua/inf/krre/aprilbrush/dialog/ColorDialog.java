3a47877893b617ba1c4a4a916de1c37bdfc1a972
Vladimir Zarypov <krre31@gmail.com>
2013-10-06 15:54:17 +0300

    colorpicker: refactoring color patterns

--- AprilBrush/src/main/java/ua/inf/krre/aprilbrush/dialog/ColorDialog.java
+++ AprilBrush/src/main/java/ua/inf/krre/aprilbrush/dialog/ColorDialog.java
@@ -7,9 +7,11 @@ import android.content.DialogInterface;
 import android.os.Bundle;
 import android.support.v4.app.DialogFragment;
 import android.view.LayoutInflater;
-import android.widget.LinearLayout;
+import android.widget.ImageView;
+import android.widget.RelativeLayout;
 
 import ua.inf.krre.aprilbrush.R;
+import ua.inf.krre.aprilbrush.logic.BrushEngine;
 
 public class ColorDialog extends DialogFragment {
 
@@ -18,10 +20,18 @@ public class ColorDialog extends DialogFragment {
         Context context = getActivity().getApplicationContext();
 
         LayoutInflater inflater = (LayoutInflater) context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
-        LinearLayout linearLayout = (LinearLayout) inflater.inflate(R.layout.colorpicker, null);
+        RelativeLayout layout = (RelativeLayout) inflater.inflate(R.layout.colorpicker, null);
+
+        int color = BrushEngine.getInstance().getColor() | 0xFF000000;
+
+        ImageView colorPrevImageView = (ImageView) layout.findViewById(R.id.colorPrevImageView);
+        colorPrevImageView.setBackgroundColor(color);
+
+        ImageView colorNewImageView = (ImageView) layout.findViewById(R.id.colorNewImageView);
+        colorNewImageView.setBackgroundColor(color);
 
         AlertDialog.Builder builder = new AlertDialog.Builder(getActivity());
-        builder.setView(linearLayout);
+        builder.setView(layout);
         builder.setPositiveButton(android.R.string.ok, new DialogInterface.OnClickListener() {
             @Override
             public void onClick(DialogInterface dialog, int which) {
