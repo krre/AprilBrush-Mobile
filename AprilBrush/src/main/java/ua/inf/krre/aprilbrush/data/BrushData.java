86eb54ce3a279634c67e6a9599ec454f73f71368
Vladimir Zarypov <krre31@gmail.com>
2013-10-06 11:21:29 +0300

    fully layout of the colorpicker

--- AprilBrush/src/main/java/ua/inf/krre/aprilbrush/data/BrushData.java
+++ AprilBrush/src/main/java/ua/inf/krre/aprilbrush/data/BrushData.java
@@ -20,6 +20,8 @@ public class BrushData {
         list = new ArrayList<Brush>();
 
         list.add(new Brush(res.getString(R.string.brush_size), 1, 150, 20));
+        list.add(new Brush(res.getString(R.string.brush_opacity), 0, 100, 90));
+        list.add(new Brush(res.getString(R.string.brush_flow), 0, 100, 25));
         list.add(new Brush(res.getString(R.string.brush_hardness), 0, 100, 80));
         list.add(new Brush(res.getString(R.string.brush_spacing), 1, 100, 15));
         list.add(new Brush(res.getString(R.string.brush_roundness), 1, 100, 100));
@@ -28,8 +30,6 @@ public class BrushData {
         list.add(new Brush(res.getString(R.string.color_hue), 0, 360, 0));
         list.add(new Brush(res.getString(R.string.color_saturation), 0, 100, 0));
         list.add(new Brush(res.getString(R.string.color_value), 0, 100, 0));
-        list.add(new Brush(res.getString(R.string.brush_opacity), 0, 100, 90));
-        list.add(new Brush(res.getString(R.string.brush_flow), 0, 100, 25));
     }
 
     public static BrushData getInstance() {
@@ -47,7 +47,7 @@ public class BrushData {
 
     // order of the elements is same as order of adding its to the list
     public enum Property {
-        SIZE, HARDNESS, SPACING, ROUNDNESS, ANGLE, SCATTER, HUE, SATURATION, VALUE, OPACITY, FLOW
+        SIZE, OPACITY, FLOW, HARDNESS, SPACING, ROUNDNESS, ANGLE, SCATTER, HUE, SATURATION, VALUE
     }
 
     public class Brush {
--- AprilBrush/src/main/java/ua/inf/krre/aprilbrush/dialog/ColorDialog.java
+++ AprilBrush/src/main/java/ua/inf/krre/aprilbrush/dialog/ColorDialog.java
@@ -8,6 +8,7 @@ import android.content.res.Resources;
 import android.os.Bundle;
 import android.support.v4.app.DialogFragment;
 import android.view.LayoutInflater;
+import android.widget.LinearLayout;
 
 import ua.inf.krre.aprilbrush.R;
 
@@ -42,11 +43,10 @@ public class ColorDialog extends DialogFragment {
         }
 */
         LayoutInflater inflater = (LayoutInflater) context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
-
+        LinearLayout linearLayout = (LinearLayout) inflater.inflate(R.layout.colorpicker, null);
 
         AlertDialog.Builder builder = new AlertDialog.Builder(getActivity());
-        builder.setView(inflater.inflate(R.layout.colorpicker, null));
-        builder.setMessage(resources.getString(R.string.action_color));
+        builder.setView(linearLayout);
         builder.setPositiveButton(android.R.string.ok, new DialogInterface.OnClickListener() {
             @Override
             public void onClick(DialogInterface dialog, int which) {
