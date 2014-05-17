d7f50459ab3fdac786a840324d009546048f28b6
Vladimir Zarypov <krre31@gmail.com>
2014-05-17 12:05:29 +0300

    setted android:largeHeap="true" in manifest

--- AprilBrush/src/main/kotlin/activity/MainActivity.kt
+++ AprilBrush/src/main/kotlin/activity/MainActivity.kt
@@ -2,10 +2,10 @@ package org.krre.aprilbrush.activity
 
 import android.app.Activity
 import android.os.Bundle
-import android.view.Menu
-import android.view.MenuItem
 import org.krre.aprilbrush.R
 import android.util.Log
+import android.app.ActivityManager
+import android.content.Context
 
 public class MainActivity() : Activity() {
     private val TAG = "AB"
@@ -13,6 +13,9 @@ public class MainActivity() : Activity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        Log.d(TAG, "test")
+
+        val activityManager = getApplicationContext()?.getSystemService(Context.ACTIVITY_SERVICE) as ActivityManager?
+        Log.d(TAG, activityManager?.getMemoryClass().toString())
+        Log.d(TAG, activityManager?.getLargeMemoryClass().toString())
     }
 }
