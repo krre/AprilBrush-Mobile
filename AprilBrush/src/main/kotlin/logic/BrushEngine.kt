aec09ed60c8c5366b25f47d3513bf942047d8d36
Vladimir Zarypov <krre31@gmail.com>
2014-05-17 16:45:25 +0300

    added BrushEngine class

---
+++ AprilBrush/src/main/kotlin/logic/BrushEngine.kt
@@ -0,0 +1,16 @@
+package org.krre.aprilbrush.logic
+
+import android.util.Log
+import android.view.MotionEvent
+
+public class BrushEngine {
+    private val TAG = "AB"
+
+    public fun paintDab(event : MotionEvent) {
+        for (i in event.getHistorySize().indices) {
+            Log.d(TAG, "history: x = ${event.getHistoricalX(i).toString()} y = ${event.getHistoricalY(i).toString()} pressure = ${event.getHistoricalPressure(i).toString()}")
+        }
+        Log.d(TAG, "x = ${event.getX().toString()} y = ${event.getY().toString()} pressure = ${event.getPressure().toString()}")
+    }
+}
+
--- AprilBrush/src/main/kotlin/view/PaintView.kt
+++ AprilBrush/src/main/kotlin/view/PaintView.kt
@@ -8,10 +8,12 @@ import android.util.AttributeSet;
 import android.view.MotionEvent;
 import android.view.View;
 import android.util.Log
+import org.krre.aprilbrush.logic.BrushEngine
 
 public class PaintView(context : Context, attrs : AttributeSet) : View(context, attrs) {
     private val bitmapPaint : Paint = Paint(Paint.DITHER_FLAG)
     private val TAG = "AB"
+    private val brushEngine = BrushEngine()
 
     override fun onSizeChanged(w : Int, h : Int, oldw : Int, oldh : Int) {
         super.onSizeChanged(w, h, oldw, oldh);
@@ -25,11 +27,11 @@ public class PaintView(context : Context, attrs : AttributeSet) : View(context, 
         when (event.getAction()) {
             MotionEvent.ACTION_DOWN -> {
                 Log.d(TAG, "down")
+                brushEngine.paintDab(event)
                 invalidate()
             }
             MotionEvent.ACTION_MOVE -> {
-                val pressure =  event.getPressure()
-                Log.d(TAG, pressure.toString())
+                brushEngine.paintDab(event)
                 invalidate()
             }
             MotionEvent.ACTION_UP -> {
