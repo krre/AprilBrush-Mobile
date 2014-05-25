6b0882320a973522c332e9b0c3027d32e8b929d6
Vladimir Zarypov <krre31@gmail.com>
2014-05-25 18:33:30 +0300

    added function to get memory class

--- AprilBrush/src/main/java/org/krre/aprilbrush/activity/MainActivity.java
+++ AprilBrush/src/main/java/org/krre/aprilbrush/activity/MainActivity.java
@@ -1,15 +1,23 @@
 package org.krre.aprilbrush.activity;
 
 import android.app.Activity;
+import android.app.ActivityManager;
+import android.content.Context;
 import android.os.Bundle;
+import android.util.Log;
 
 import org.krre.aprilbrush.R;
 
 public class MainActivity extends Activity {
+    private int memoryClass;
+    private String TAG = "AB";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main_activity);
+
+        ActivityManager activityManager = (ActivityManager)getBaseContext().getSystemService(Context.ACTIVITY_SERVICE);
+        memoryClass = activityManager.getLargeMemoryClass();
     }
 }
