16d1f4e248ea923ac710669d49dcdf21757f133f
Vladimir Zarypov <krre31@gmail.com>
2014-06-05 12:25:25 +0300

    added slider bars to brush settings fragment

--- AprilBrush/src/main/java/org/krre/aprilbrush/activity/BrushSettingsFragment.java
+++ AprilBrush/src/main/java/org/krre/aprilbrush/activity/BrushSettingsFragment.java
@@ -2,16 +2,43 @@ package org.krre.aprilbrush.activity;
 
 import android.app.Fragment;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.LinearLayout;
 
 import org.krre.aprilbrush.R;
+import org.krre.aprilbrush.view.SliderView;
 
 public class BrushSettingsFragment extends Fragment {
+    private final String TAG = "AB";
+
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.brush_settings, null);
+        ViewGroup scrollView = (ViewGroup)inflater.inflate(R.layout.brush_settings, null);
+        LinearLayout layout = (LinearLayout)scrollView.getChildAt(0);
+
+//        for (int i = 0; i < brushEngine.getBrushList().length; i++) {
+        for (int i = 0; i < 15; i++) {
+            SliderView sliderView = new SliderView(getActivity().getBaseContext());
+//            BrushData.Brush brush = brushData.getList().get(i);
+
+//            sliderView.setName(brush.getName());
+            sliderView.setName("name");
+//            sliderView.setMin(brush.getMinValue());
+            sliderView.setMin(0);
+//            sliderView.setMax(brush.getMaxValue());
+            sliderView.setMax(100);
+//            sliderView.setValue(brushEngine.getValue(i));
+            sliderView.setValue(20);
+            sliderView.setId(i);
+//            sliderView.addObserver(brushEngine);
+
+            layout.addView(sliderView);
+        }
+
+        return scrollView;
     }
 }
---
+++ AprilBrush/src/main/java/org/krre/aprilbrush/view/SliderView.java
@@ -0,0 +1,106 @@
+package org.krre.aprilbrush.view;
+
+import android.content.Context;
+import android.util.AttributeSet;
+import android.view.LayoutInflater;
+import android.widget.RelativeLayout;
+import android.widget.SeekBar;
+import android.widget.TextView;
+
+import java.util.Observable;
+import java.util.Observer;
+
+import org.krre.aprilbrush.R;
+
+public class SliderView extends RelativeLayout {
+    protected SeekBar seekBar;
+    private TextView nameView;
+    private TextView valueView;
+    private int minValue;
+    private int value;
+    private int id;
+    private SliderObservable sliderObservable = new SliderObservable();
+
+    public SliderView(Context context) {
+        super(context);
+        initComponent();
+    }
+
+    public SliderView(Context context, AttributeSet attrs) {
+        super(context, attrs);
+        initComponent();
+    }
+
+    public SeekBar getSeekBar() {
+        return seekBar;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public int getValue() {
+        return value;
+    }
+
+    public void setValue(int value) {
+        seekBar.setProgress(value);
+        valueView.setText(String.format("%d", value));
+    }
+
+    public String getName() {
+        return nameView.getText().toString();
+    }
+
+    public void setName(String name) {
+        nameView.setText(name);
+    }
+
+    public void setMax(int value) {
+        seekBar.setMax(value - minValue);
+    }
+
+    public void setMin(int value) {
+        this.minValue = value;
+    }
+
+    public void addObserver(Observer observer) {
+        sliderObservable.addObserver(observer);
+    }
+
+    private void initComponent() {
+        LayoutInflater inflater = (LayoutInflater) getContext().getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+        inflater.inflate(R.layout.brush_slider, this, true);
+        nameView = (TextView) findViewById(R.id.sliderName);
+        valueView = (TextView) findViewById(R.id.sliderValue);
+
+        seekBar = (SeekBar) findViewById(R.id.sliderSeekBar);
+        seekBar.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar1) {
+            }
+
+            @Override
+            public void onProgressChanged(SeekBar seekBar1, int progress, boolean fromUser) {
+                value = minValue + progress;
+                valueView.setText(String.format("%d", value));
+                sliderObservable.onValueChanged(SliderView.this);
+            }
+
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar1) {
+            }
+        });
+    }
+
+    public class SliderObservable extends Observable {
+        public void onValueChanged(SliderView sliderView) {
+            setChanged();
+            notifyObservers(sliderView);
+        }
+    }
+}
