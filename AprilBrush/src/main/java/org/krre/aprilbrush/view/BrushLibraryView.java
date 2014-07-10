a1a5d112a8fc7b6cc5b45a82be176fe5b19dfb82
Vladimir Zarypov <krre31@gmail.com>
2014-07-10 12:41:00 +0300

    added LoaderCallbacks to BrushLibraryView class

--- AprilBrush/src/main/java/org/krre/aprilbrush/view/BrushLibraryView.java
+++ AprilBrush/src/main/java/org/krre/aprilbrush/view/BrushLibraryView.java
@@ -1,22 +1,28 @@
 package org.krre.aprilbrush.view;
 
+import android.app.LoaderManager;
 import android.content.ContentValues;
 import android.content.Context;
+import android.content.CursorLoader;
+import android.content.Loader;
 import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
+import android.os.Bundle;
 import android.util.AttributeSet;
 import android.widget.ListView;
+import android.widget.SimpleCursorAdapter;
 
 import org.krre.aprilbrush.R;
 import org.krre.aprilbrush.logic.BrushDbHelper;
 
 import java.util.ArrayList;
 
-public class BrushLibraryView extends ListView {
+public class BrushLibraryView extends ListView implements LoaderManager.LoaderCallbacks<Cursor> {
     private static final String TAG = "AB";
     private BrushDbHelper brushDbHelper;
     private SQLiteDatabase db;
     private Context context;
+    private SimpleCursorAdapter adapter;
 
     public BrushLibraryView(Context context, AttributeSet attrs) {
         super(context, attrs);
@@ -24,6 +30,12 @@ public class BrushLibraryView extends ListView {
         this.context = context;
         brushDbHelper = new BrushDbHelper(context);
         db = brushDbHelper.getWritableDatabase();
+        String[] from = new String[] { "name" };
+        int[] to = new int[] { R.id.brushlibTextView };
+        adapter = new SimpleCursorAdapter(context, R.layout.brushlib_item, null, from, to, 0);
+        setAdapter(adapter);
+//        getSupportLoaderManager().initLoader(0, null, this);
+
     }
 
     public void add(ArrayList<SliderView> sliderViews) {
@@ -43,5 +55,24 @@ public class BrushLibraryView extends ListView {
         }
 
         db.insert(BrushDbHelper.TABLE, null, cv);
+    }
+
+    @Override
+    public void onLoadFinished(Loader<Cursor> loader, Cursor cursor) {
+        adapter.swapCursor(cursor);
+    }
+
+    @Override
+    public Loader<Cursor> onCreateLoader(int id, Bundle bundle) {
+        return new CursorLoader(context) {
+            @Override
+            public Cursor loadInBackground() {
+                return db.query(BrushDbHelper.TABLE, null, null, null, null, null, null);
+            }
+        };
+    }
+
+    @Override
+    public void onLoaderReset(Loader<Cursor> loader) {
     }
 }
